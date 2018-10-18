ffe1fadcd3103a82446c805efb05db57997c61fa
declipz <declipz1121@gmail.com>
2018-10-18 15:39:54 +0300

    init commit

---
+++ CryptographyLAB2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CryptographyLAB2
+//
+//  Created by Vladislav Kondrashkov on 10/18/18.
+//  Copyright © 2018 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CryptographyLAB2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CryptographyLAB2
+//
+//  Created by Vladislav Kondrashkov on 10/18/18.
+//  Copyright © 2018 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
